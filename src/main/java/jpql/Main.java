466495b2a2a08ebf68ac974b0f2bb345f7d2f379
pyoh07 <pyoh07@gmail.com>
2025-10-14 15:24:16 +0900

    기본 문법과 쿼리 API

--- src/main/java/jpql/Main.java
+++ src/main/java/jpql/Main.java
@@ -25,32 +25,6 @@ public class Main {
             member.setAge(10);
             em.persist(member);
 
-            //Type Query : 반환타입 명확
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            //Query : 반환타입 명확하지 않음
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
-
-            //결과가 하나 이상 일경우 - 결과가 없으면 빈 리스트 반환
-            List<Member> resultList = query1.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
-            //단일값일 경우 - 결과가 정확히 하나.
-            // 표준스펙 : 결과가 없으면 NoResultException, 결과가 둘이상이면 NonUniqueResultException 발생
-            //Spring Data JPA -> 결과가 없으면 Null 이나 Optional
-            Member memberA = query1.getSingleResult();
-
-            //파라미터 바인딩
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username=:username", Member.class);
-            query.setParameter("username", "member1");
-            Member singleResult = query.getSingleResult();
-            System.out.println("singleResult = "+ singleResult.getUsername());
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                            .setParameter("username", "member1")
-                            .getSingleResult();
-            System.out.println("singleResult = "+ singleResult.getUsername());
-
 
             tx.commit();
         } catch (Exception e){
